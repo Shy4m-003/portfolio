121e6fe650024f1b0462d4b7a0eb1ab12885d3eb
Shy4m-003 <shyamprasadreddyginkala@gmail.com>
2025-07-28 19:37:49 +0530

    Finalize responsive portfolio layout with complete sections and deploy-ready structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, FooterComponent,HeroComponent,AboutComponent,ProjectsComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
